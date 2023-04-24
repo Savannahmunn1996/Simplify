f825f2c467366173a6f49b07232fca95cc478698
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-04-24 14:51:49 -0700

    Set up files/components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Hero from './components/Hero';
+import Summary from './components/Summary';
+
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
