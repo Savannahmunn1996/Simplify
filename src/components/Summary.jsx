db63ea89923b714d1bad64f55c0ad14403e3a014
Savannah Mercedes <savannahmunn1996@gmail.com>
2023-04-24 19:31:11 -0700

    adding style

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useState, UseEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Summary = () => {
   return (
-    <div></div>
+    <section className="mt16 w-full max-w-xl">
+      <div className='flex flex-col w-full gap-2'>
+        <form className="relative flex justify-center items-center" onSubmit={() => { }}>
+          <img src={linkIcon} alt='link_icon' className='absolute left-0 y-2 ml-3 w-5 pt-36'></img>
+          <input type='url' placeholder='Enter any article url' value=""
+            onChange={() => { }} required className='url_input  mt-36 ' ></input>
+            <button type="submit" className=" mt-36 bg-blue-400 w-40 h-10 font-chakra rounded-xl text-white right-0 my-1.5 mr-1.5 ml-8 flex  items-center justify-center">Summarize</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
